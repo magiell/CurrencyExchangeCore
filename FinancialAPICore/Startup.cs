494fd19b33b2a3dca71e686f5cea3618b219df7e
magiell <magiell@naver.com>
2018-11-05 10:43:53 +0900

    add to swagger

--- FinancialAPICore/Startup.cs
+++ FinancialAPICore/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace FinancialAPICore
 {
@@ -27,8 +28,25 @@ namespace FinancialAPICore
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
             services.AddMemoryCache();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info {
+                    Title = "API",
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Magiell",                                                
+                    },
+                    //License = new License
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = "https://example.com/license"
+                    //}
+                });                
+            });
             //services.AddDbContext<CurrencyExchangeContext>(opt => opt.UseInMemoryDatabase("Currency"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -44,7 +62,11 @@ namespace FinancialAPICore
             {
                 app.UseHsts();
             }
-            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/v1/swagger.json", "API v1");
+                c.RoutePrefix = string.Empty;                
+            });
             //app.UseHttpsRedirection();
             app.UseDefaultFiles();
             app.UseStaticFiles();
